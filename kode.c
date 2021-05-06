6449562432918ed100ffc5e9fc53b9bd0b174523
EmpereurRa <64724312+EmpereurRa@users.noreply.github.com>
2021-05-06 17:41:28 +0300

    Update kode.c

--- kode.c
+++ kode.c
@@ -15,8 +15,7 @@ int main (){
 	al_register_event_source(queue, al_get_keyboard_event_source());
 	al_register_event_source(queue, al_get_display_event_source(disp));
 	al_register_event_source(queue, al_get_timer_event_source(timer));
-	
-        
+
         bool done = false;
         bool redraw = true;
         int lefttoright = 0;
